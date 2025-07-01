f0986a59aec6a551c0982e81dcea8115b67beec7
komorra <komorra@gmail.com>
2025-07-01 18:08:47 +0200

    Refactor SATSolver threading and stream management
    Reduced FastSolverThreadCount from 8 to 4 and replaced private stream size fields with computed properties. Added explicit management of solvingStreams and variables, implemented multi-threaded solver initialization, and improved thread logic for SAT solving.

--- SATStreams/SATSolver.cs
+++ SATStreams/SATSolver.cs
@@ -18,16 +18,23 @@ namespace SATStreams
         /// </summary>
         public bool UseCheckpoints { get; set; } = true;
         public int SlowSolverThreadCount { get; set; } = 1;
-        public int FastSolverThreadCount { get; set; } = 8;
+        public int FastSolverThreadCount { get; set; } = 4;
         public int SlowSolverTimeOut { get; set; } = 60000; // in milliseconds
         public int FastSolverTimeOut { get; set; } = 10000; // in milliseconds
         public string ProblemName { get; set; } = "SATSolver";
 
         public event Action<string> LogMessage;
 
-        public int LowestStreamSize { get; private set; }
-        public int HighestStreamSize { get; private set; }
-        public int StreamCount { get; private set; }
+        public int LowestStreamSize => solvingStreams.Count > 0 ? solvingStreams.Min(x => x.Clause.Count) : 0;
+        public int HighestStreamSize => solvingStreams.Count > 0 ? solvingStreams.Max(x => x.Clause.Count) : 0;
+        public int StreamCount => solvingStreams.Count;
+        public int SolvedVars => init.Count;
+        public int TotalVars => variables.Count;
+
+
+        private List<SATStream> solvingStreams = new List<SATStream>();
+        private Clause variables;
+        private Random random = new Random(42);
 
         public SATSolver(CNF cnf, string problemName = null)
         {
@@ -36,6 +43,7 @@ namespace SATStreams
             this.cnf = cnf;
             this.lut = Utils.CreateLUT(cnf);
             this.ProblemName = problemName ?? "SATSolver";
+            this.variables = Utils.GetVariables(cnf);
 
             var units = cnf.Where(x => x.Count == 1).SelectMany(x => x).ToHashSet();
             init = Utils.OutCome(new Clause(), units, lut);
@@ -48,14 +56,62 @@ namespace SATStreams
         {
             var rootStream = new SATStream();
             rootStream.Clause = new Clause(init);
+            solvingStreams.Add(rootStream);
+
+            for (int i = 0; i < FastSolverThreadCount; i++)
+            {
+                var thread = new Thread(() => SolverThread(random.Next(), FastSolverTimeOut));
+                thread.IsBackground = true;
+                thread.Start();
+            }
+            for (int i = 0; i < SlowSolverThreadCount; i++)
+            {
+                var thread = new Thread(() => SolverThread(random.Next(), SlowSolverTimeOut));
+                thread.IsBackground = true;
+                thread.Start();
+            }
 
 
-            return null;
+            while (init.Count < variables.Count)
+            {
+
+            }
+
+            return init;
         }
 
-        public void SolverThread()
+        private void SolverThread(int seed, int timeOut)
         {
+            var random = new Random(seed);
+            var solver = new Z3Solver(cnf, timeOut);
 
+            while(true)
+            {
+                try
+                {
+                    var randomStream = solvingStreams                        
+                        .OrderBy(x => random.Next())
+                        .FirstOrDefault();
+
+                    if(randomStream.IsMarkedForDeletion)
+                    {
+                        Thread.Sleep(100);
+                        continue;
+                    }
+
+                    var check = solver.Solve(randomStream.Clause, out var solution);
+                    if (check == false)
+                    {
+                        randomStream.IsMarkedForDeletion = true;
+                    }
+                    else if(check == true)
+                    {
+                        init = solution;
+                        return;
+                    }
+                }
+                catch { }
+            }
         }
     }
 }
